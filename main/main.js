1f5b365ef3bfe537d285b125d78d08fa04c5d5ed
LiMiaoJing <466020917@qq.com>
2017-06-03 00:37:38 +0800

    Check the email suffix

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var splits = email.split("@");
+  for(var i = 0; i < suffixes.length; i++) {
+      if(splits[1] == suffixes[i]) {
+        return true;
+      }
+  }
+  return false;
 };
